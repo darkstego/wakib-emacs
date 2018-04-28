fb4db3dd20e9a27c3489a0257e0f58eec84a1735
Abdulla Bubshait <darkstego@gmail.com>
2018-04-28 19:55:58 -0400

    Initial commit

---
+++ init.el
@@ -0,0 +1,115 @@
+;; -*- lexical-binding: t -*-
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;;(package-initialize)
+
+(setq debug-on-error t)
+
+(add-to-list 'load-path (expand-file-name "local" user-emacs-directory))
+(add-to-list 'load-path "/home/bubshait/code/emacs/wakib-mode")
+
+(require 'wakib-mode)
+(wakib-mode 1)
+
+
+;;----------------------------------------------------------------------------
+;; Adjust garbage collection thresholds during startup, and thereafter
+;;----------------------------------------------------------------------------
+;(let ((normal-gc-cons-threshold (* 20 1024 1024))
+;      (init-gc-cons-threshold (* 128 1024 1024)))
+;  (setq gc-cons-threshold init-gc-cons-threshold) 
+;  (add-hook 'after-init-hook		
+					;            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+(setq gc-cons-threshold (* 20 1024 1024))
+
+
+;; -----------------------
+;; use-package
+;; -----------------------
+(setq load-prefer-newer t)              ; Don't load outdated byte code
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+;; -------------------
+;; Theme
+;; -------------------
+(use-package moe-theme
+  :config
+  (load-theme 'moe-dark t))
+
+;; -------------------
+;; Magit
+;; -------------------
+(use-package magit
+  :bind
+  (("C-x g" . magit-status )))
+
+
+;; -------------------
+;; expand-region
+;; -------------------
+(use-package expand-region
+  :commands er/expand-region
+  :bind ("C-S-a" . er/expand-region))
+
+;; -------------------
+;; uniquify
+;; -------------------
+
+;; TODO (built into emacs, check performance hit if worth it)
+
+
+
+(use-package quickrun
+  :config
+  (setq quickrun-focus-p nil)
+  :bind
+  (([f8] . quickrun )))
+
+
+;; Better Parenthesis
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+(show-paren-mode 1)
+(electric-pair-mode 1)
+;; TODO - MOVE Electric Pair Mode to user local
+
+;; Setup Splash Screen
+(setq inhibit-startup-screen t)
+
+
+(provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (moe-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
